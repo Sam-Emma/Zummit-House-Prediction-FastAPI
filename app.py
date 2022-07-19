1ebc5746872276585a80991185ecad16fe6e5232
Samuel Emmanuel <58962390+Sam-Emma@users.noreply.github.com>
2022-07-19 17:30:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,14 @@
-# 1. Library imports
+
 import uvicorn
 from fastapi import FastAPI
 from House import House
 import pickle
-# 2. Create the app object
+
 app = FastAPI()
 pickle_in = open("model.sav","rb")
 classifier=pickle.load(pickle_in)
-# import nest_asyncio
-# nest_asyncio.apply()
 
-# # 3. Index route, opens automatically on http://127.0.0.1:8000
-# @app.get('/')
-# async def index():
-#     return {'message': 'Hello, World'}
 
-# # 4. Route with a single parameter, returns the parameter within a message
-# #    Located at: http://127.0.0.1:8000/AnyNameHere
-
-# @app.get('/{name}')
-# async def get_name(name: str):
-#     return {'Welcome To Krish Youtube Channel': f'{name}'}
-
-# # 3. Expose the prediction functionality, make a prediction from the passed
-# #     JSON data and return the predicted Bank Note with the confidence
-
-#from datetime import date
 @app.post('/')
 def predict_house_price(data:House):
     year=data.date.year
@@ -75,9 +58,7 @@ def predict_house_price(data:House):
         'prediction': prediction
     }
 
-# 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
     uvicorn.run(app)
     
-#uvicorn app:app --reload
